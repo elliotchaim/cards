e463b693872636a628088d4251cde7e90a7dffe9
elliotchaim <elliotchaim@users.noreply.github.com>
2016-08-08 06:49:28 +0100

    added Windsor IoC Containter

--- CardGame/Global.asax.cs
+++ CardGame/Global.asax.cs
@@ -1,14 +1,33 @@
 ﻿namespace CardGame
 {
-    using System.Web.Mvc;
-    using System.Web.Routing;
+   using Infrastructure;
+   using System.Web.Mvc;
+   using System.Web.Routing;
+   using Castle.Windsor;
+   using Castle.Windsor.Installer;
 
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+   public class MvcApplication: System.Web.HttpApplication
+   {
+      private static IWindsorContainer container;
+
+      protected void Application_Start()
+      {
+         AreaRegistration.RegisterAllAreas();
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+         BootstrapWindsorContainer();
+      }
+
+      protected void Application_End()
+      {
+         container.Dispose();
+      }
+
+      private static void BootstrapWindsorContainer()
+      {
+         container = new WindsorContainer().Install(FromAssembly.This());
+         var controllerFactory = new WindsorControllerFactory(container.Kernel);
+         ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+      }
+   }
 }
